7e0ba182d11b50ae63be44ceaabb59c6a3558a96
jasc1204 <185645269+jasc1204@users.noreply.github.com>
2025-02-23 12:57:14 -0500

    html done I think

---
+++ javasito.js
@@ -0,0 +1,29 @@
+let total = 0;
+
+
+function add(numberA,numberB)
+{
+    return numberA+numberB;
+}
+
+function subtract(numberA,numberB)
+{
+    return numberA-numberB
+}
+
+function multiply(numberA,numberB)
+{
+    return numberA * numberB;
+}
+
+function divide(numberA,numberB)
+{
+    return numberA / numberB;
+}
+
+
+
+function operator(numberA,operator,numberB)
+{
+    
+}
